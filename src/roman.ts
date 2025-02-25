af26029b1971cf106117ae9c1095c1bb3b65aa11
Patchara Suwanwanakul <patcharas@cariva.co.th>
2025-02-25 13:52:50 +0700

    refactor code for next test case

--- src/roman.ts
+++ src/roman.ts
@@ -1,12 +1,29 @@
-export function convertRomanStringToDecimalNumber(roman: string): number {
-  let decimalNumber: number = -1
 
-  if (roman === 'I') {
-    decimalNumber = 1
-  } else if (roman === 'II') {
-    decimalNumber = 2
-  } else if (roman === 'III') {
-    decimalNumber = 3
+
+export function convertRomanStringToDecimalNumber(roman: string): number {
+  const keyMapping: Record<string, number> = { "I": 1 };
+  const errorValue = -1
+
+  const isEmptyString = (str: string) => {
+    return str.trim().length === 0
+  }
+
+  const isNotValidString = (str: string, whitelists: string[]) => {
+    return str.split("").some(c => !whitelists.includes(c))
+  }
+
+  if (isEmptyString(roman) || isNotValidString(roman, Object.keys(keyMapping))) {
+    return errorValue
+  }
+
+  let decimalNumber: number = 0
+
+  const chars = roman.split("")
+
+  for (const c of chars) {
+    if (c == "I") {
+      decimalNumber += 1
+    }
   }
 
   return decimalNumber
